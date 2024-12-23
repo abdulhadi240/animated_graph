e1e220b40750f1bbadb57bac925c3199fa6d8d4f
abdulhadi240 <ah912425@gmail.com>
2024-12-23 12:50:23 +0500

    a

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,6 +24,9 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <div className="flourish-embed flourish-bar-chart-race" data-src="visualisation/20916827"></div>
+        <Script src="https://public.flourish.studio/resources/embed.js"></Script>
+        
       </body>
     </html>
   );
